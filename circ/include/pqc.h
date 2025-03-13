5edb04486c624204db53174338a19e4155818786
Timo Ziegler <timo@ip75.237.mip.uni-hannover.de>
2025-03-13 16:12:47 +0100

    gradPQC fails unit test

--- circ/include/pqc.h
+++ circ/include/pqc.h
@@ -28,8 +28,8 @@ extern "C" {
  * =====================================================================================================================
  */
 /*
- * Function: Apply Parametrized Quantum Block (PQB)
- * -------------------------------------------------
+ * Function: Apply Quantum Block (QB)
+ * ------------------------------------
  * This function applies a fixed sequence of gates to a quantum state
  */
 typedef void (*applyQB)(state_t* state);
@@ -42,11 +42,19 @@ typedef void (*applyQB)(state_t* state);
 typedef void (*applyPQB)(state_t* state, double par);
 
 /*
+ * Function: Apply Linear Combination of Quantum Blocks (LCQB)
+ * -----------------------------------------------------------
+ * This function applies a linear combination of (parametrized) gates to a quantum state
+ */
+typedef void (*applyLCQB)(state_t* state, double c[]);
+
+/*
  * =====================================================================================================================
  *                                                  Function definitions
  * =====================================================================================================================
  */
-void evolveQB(state_t* state, applyQB qb, double par);
+void evoQB(state_t* state, applyQB qb, double par);
+void lcQB(state_t* state, applyQB qb[], double c);
 void applyPQC(state_t* state, depth_t d, const applyPQB pqbs[], const double par[]);
 
 #ifdef __cplusplus
