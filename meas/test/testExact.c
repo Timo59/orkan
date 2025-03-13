610f7a4154c7839020b1607fb920835c5b96e32f
Timo Ziegler <timo@ip75.237.mip.uni-hannover.de>
2025-03-13 16:12:47 +0100

    gradPQC fails unit test

--- meas/test/testExact.c
+++ meas/test/testExact.c
@@ -6,8 +6,8 @@
  *                                                      includes
  * =====================================================================================================================
  */
-#ifndef PQC_H
-#include "pqc.h"
+#ifndef EXACT_H
+#include <exact.h>
 #endif
 
 #ifndef QHIPSTER_H
@@ -31,9 +31,10 @@
  *                                                      macros
  * =====================================================================================================================
  */
-#define PRECISION               1e-8
+#define APPROXPRECISION         1e-4
+#define EPSILON                 1e-9
 #define MAXQUBITS               6
-#include <exact.h>
+#define PRECISION               1e-8
 
 /*
  * =====================================================================================================================
@@ -75,7 +76,7 @@ void testMeanObs(void) {
                 ref += pow(cabs(vecs[i][j]), 2) * diagObs[j];
             }
 
-            TEST_ASSERT_TRUE(cvectorAlmostEqual(vecs[i], testState.vec, dim, PRECISION));
+            TEST_ASSERT_TRUE(ralmostEqual(ref, test, PRECISION));
         }
         freeTestVectors(vecs, qubits);
         stateFreeVector(&testState);
@@ -202,7 +203,258 @@ void rswap6(state_t* state, const double par) {
 applyPQB pqbs[20] = {rx2, ry2, rz2, rswap2, rx3, ry3, rz3, rswap3, rx4, ry4, rz4, rswap4, rx5, ry5, rz5, rswap5, \
     rx6, ry6, rz6, rswap6};
 
+void x2(state_t* state) {
+    applyX(state, 0);
+    applyX(state, 1);
+}
+void y2(state_t* state) {
+    applyY(state, 0);
+    applyY(state, 1);
+}
+void z2(state_t* state) {
+    applyZ(state, 0);
+    applyZ(state, 1);
+}
+void swap2(state_t* state) {
+    applySWAP(state, 0, 1);
+}
+void x3(state_t* state) {
+    applyX(state, 0);
+    applyX(state, 1);
+    applyX(state, 2);
+}
+void y3(state_t* state) {
+    applyY(state, 0);
+    applyY(state, 1);
+    applyY(state, 2);
+}
+void z3(state_t* state) {
+    applyZ(state, 0);
+    applyZ(state, 1);
+    applyZ(state, 2);
+}
+void swap3(state_t* state) {
+    applySWAP(state, 0, 1);
+}
+void x4(state_t* state) {
+    applyX(state, 0);
+    applyX(state, 1);
+    applyX(state, 2);
+    applyX(state, 3);
+}
+void y4(state_t* state) {
+    applyY(state, 0);
+    applyY(state, 1);
+    applyY(state, 2);
+    applyY(state, 3);
+}
+void z4(state_t* state) {
+    applyZ(state, 0);
+    applyZ(state, 1);
+    applyZ(state, 2);
+    applyZ(state, 3);
+}
+void swap4(state_t* state) {
+    applySWAP(state, 0, 1);
+    applySWAP(state, 2, 3);
+}
+void x5(state_t* state) {
+    applyX(state, 0);
+    applyX(state, 1);
+    applyX(state, 2);
+    applyX(state, 3);
+    applyX(state, 4);
+}
+void y5(state_t* state) {
+    applyY(state, 0);
+    applyY(state, 1);
+    applyY(state, 2);
+    applyY(state, 3);
+    applyY(state, 4);
+}
+void z5(state_t* state) {
+    applyZ(state, 0);
+    applyZ(state, 1);
+    applyZ(state, 2);
+    applyZ(state, 3);
+    applyZ(state, 4);
+}
+void swap5(state_t* state) {
+    applySWAP(state, 0, 1);
+    applySWAP(state, 2, 3);
+}
+void x6(state_t* state) {
+    applyX(state, 0);
+    applyX(state, 1);
+    applyX(state, 2);
+    applyX(state, 3);
+    applyX(state, 4);
+    applyX(state, 5);
+}
+void y6(state_t* state) {
+    applyY(state, 0);
+    applyY(state, 1);
+    applyY(state, 2);
+    applyY(state, 3);
+    applyY(state, 4);
+    applyY(state, 5);
+}
+void z6(state_t* state) {
+    applyZ(state, 0);
+    applyZ(state, 1);
+    applyZ(state, 2);
+    applyZ(state, 3);
+    applyZ(state, 4);
+    applyZ(state, 5);
+}
+void swap6(state_t* state) {
+    applySWAP(state, 0, 1);
+    applySWAP(state, 2, 3);
+    applySWAP(state, 4, 5);
+}
+
+applyQB qbs[20] = {x2, y2, z2, swap2, x3, y3, z3, swap3, x4, y4, z4, swap4, x5, y5, z5, swap5, x6, y6, z6, swap6};
+
 cplx_t* (*rMat[])(qubit_t, qubit_t, double) = {RXGateMat, RYGateMat, RZGateMat};
+
+void testGradPQC(void) {
+    state_t testState;
+    for (qubit_t qubits = 2; qubits < MAXQUBITS; qubits++) {
+        const dim_t dim = POW2(qubits, dim_t);
+        stateInitEmpty(&testState, qubits);
+        cplx_t** vecs = generateTestVectors(qubits);
+
+        /*
+         * Define reference matrices in an array holding the addresses of the PQBs holding the addresses of matrices
+         */
+        cplx_t** pqbMat[4];
+        for (uint8_t i = 0; i < 3; i++) {
+            if ((pqbMat[i] = malloc(qubits * sizeof (cplx_t*))) == NULL) {
+                fprintf(stderr, "testGradPQC: pqbMat[%d] allocation failed\n", i);
+                for (uint8_t j = 0; j < i; j++) {
+                    for (uint8_t k = 0; k < qubits; k++) {          // Free all matrix memory allocated up to current i
+                        free(pqbMat[j][k]);
+                    }
+                    free(pqbMat[j]);                                // Free all matrix memory address space allocated
+                }                                                   // up to current i
+                freeTestVectors(vecs, qubits);
+                stateFreeVector(&testState);
+                exit(EXIT_FAILURE);
+            }
+        }
+        const uint8_t swapc = qubits / 2;                           // Number of adjacent swaps
+        if ((pqbMat[3] = malloc(swapc * sizeof (cplx_t*))) == NULL) {
+            fprintf(stderr, "testGradPQC: pqbMat[3] allocation failed\n");
+            for (uint8_t j = 0; j < 3; j++) {
+                for (uint8_t k = 0; k < qubits; k++) {              // Free all matrix memory allocated for Pauli
+                    free(pqbMat[j][k]);                             // rotations
+                }
+                free(pqbMat[j]);                                    // Free all matrix memory address space allocated
+            }                                                       // for Pauli rotations
+        }
+
+        /* TESTING */
+        for (dim_t i = 0; i < dim + 1; i++) {
+            stateInitVector(&testState, vecs[i]);
+            double* test = NULL;
+            gradPQC(&testState, 4, pqbs + 4 * (qubits - 2), randPar, qbs + 4 * (qubits - 2), diagObs, &test);
+
+            double ref[4];
+            cplx_t* tmp = malloc(dim * sizeof (cplx_t));
+            if (!tmp) {
+                fprintf(stderr, "testGradPQC: tmp allocation failed\n");
+                return;
+            }
+            for (dim_t j = 0; j < dim; j++) {                       // Copy current test vector to tmp
+                tmp[j] = vecs[i][j];
+            }
+
+            for (uint8_t j = 0; j < 3; j++) {
+                for (uint8_t k = 0; k < qubits; k++) {              // Initialize Pauli rotations with current parameter
+                    pqbMat[j][k] = rMat[j](qubits, k, randPar[j]);
+                }
+            }
+            for (uint8_t j = 0; j < swapc; j++) {                   // Initialize SWAP rotation with current parameters
+                pqbMat[3][j] = RswapGateMat(qubits, 2 * j, 2 * j + 1, randPar[3]);
+            }
+
+            for (uint8_t j = 0; j < 3; j++) {
+                for (uint8_t k = 0; k < qubits; k++) {              // Multiply tmp by Pauli rotations
+                    cmatVecMulInPlace(pqbMat[j][k], tmp, dim);
+                }
+            }
+            for (uint8_t j = 0; j < swapc; j++) {                   // Multiply tmp by SWAP rotations
+                cmatVecMulInPlace(pqbMat[3][j], tmp, dim);
+            }
+
+            double mean = 0;                                        // Mean value of tmp with the current parameters
+            for (dim_t j = 0; j < dim; j++) {
+                mean += pow(cabs(tmp[j]), 2) * diagObs[j];
+            }
+
+            double ref2[4];
+            stateInitVector(&testState, vecs[i]);
+            applyPQC(&testState, 4, pqbs, randPar);
+            double meanTest = meanObs(&testState, diagObs);
+
+            for (depth_t j = 0; j < 4; ++j) {
+                randPar[j] += EPSILON;                              // Slightly shift the j-th parameter value
+
+                stateInitVector(&testState, vecs[i]);
+                applyPQC(&testState, 4, pqbs, randPar);
+                ref2[j] = 1. / EPSILON * (meanObs(&testState, diagObs) - meanTest);
+
+                for (dim_t k= 0; k < dim; k++) {                    // Copy current test vector to tmp
+                    tmp[k] = vecs[i][k];
+                }
+
+                for (uint8_t k = 0; k < 3; k++) {
+                    for (uint8_t l = 0; l < qubits; l++) {          // Pauli rotations with shifted parameter
+                        pqbMat[k][l] = rMat[k](qubits, l, randPar[k]);
+                    }
+                }
+                for (uint8_t k = 0; k < swapc; k++) {               // SWAP rotation with shifted parameters
+                    pqbMat[3][k] = RswapGateMat(qubits, 2 * k, 2 * k + 1, randPar[3]);
+                }
+
+                for (uint8_t k = 0; k < 3; k++) {
+                    for (uint8_t l = 0; l < qubits; l++) {          // Multiply tmp by Pauli rotations
+                        cmatVecMulInPlace(pqbMat[k][l], tmp, dim);
+                    }
+                }
+                for (uint8_t k = 0; k < swapc; k++) {               // Multiply tmp by SWAP rotations
+                    cmatVecMulInPlace(pqbMat[3][k], tmp, dim);
+                }
+
+                double val = 0;                                     // Mean value of tmp with the shifted parameters
+                for (dim_t k = 0; k < dim; k++) {
+                    val += pow(cabs(tmp[k]), 2) * diagObs[k];
+                }
+                ref[j] = 1. / EPSILON * (val - mean);
+                randPar[j] -= EPSILON;
+            }
+            free(tmp);
+
+            printf("test = ");
+            vectorPrint(test, 4);
+            printf("ref = ");
+            vectorPrint(ref, 4);
+            printf("ref = ");
+            vectorPrint(ref2, 4);
+            TEST_ASSERT_TRUE(rvectorAlmostEqual(ref, test, 4, APPROXPRECISION));
+        }
+        for (uint8_t j = 0; j < 3; j++) {
+            for (uint8_t k = 0; k < qubits; k++) {              // Free all allocated matrix memory
+                free(pqbMat[j][k]);
+            }
+            free(pqbMat[j]);                                    // Free all allocated matrix memory address space
+        }
+        for (uint8_t j = 0; j < swapc; j++) {
+            free(pqbMat[3][j]);
+        }
+        free(pqbMat[3]);
+    }
+}
 
 /*
  * =====================================================================================================================
@@ -212,5 +464,6 @@ cplx_t* (*rMat[])(qubit_t, qubit_t, double) = {RXGateMat, RYGateMat, RZGateMat};
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(testMeanObs);
+    RUN_TEST(testGradPQC);
     return UNITY_END();
 }
